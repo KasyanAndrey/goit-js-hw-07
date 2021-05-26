5a8c535e8c1c1d1f119b8a2abe6f187e464ec5f3
KasyanAndrey <andreykasyan88@gmail.com>
2021-05-26 17:50:13 +0300

    done task-03

---
+++ js/task-03.js
@@ -0,0 +1,46 @@
+/* Задание 3
+Напиши скрипт для создания галлереи изображений по массиву данных.
+
+В HTML есть список ul#gallery.
+
+<ul id="gallery"></ul>
+Используй массив объектов images для создания тегов img вложенных в li. 
+Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+ */
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const makeGalleryItemMarkup = images => {
+  const { url, alt } = images;
+  return `
+  <li>
+    <img src="${url}" alt="${alt}">
+  </li>
+  `;
+};
+
+console.log(images);
+console.log(makeGalleryItemMarkup(images[2]));
+
+const galleryEl = document.querySelector('#gallery');
+
+const makeGalleryItems = images.map(makeGalleryItemMarkup).join('');
+galleryEl.insertAdjacentHTML('beforeend', makeGalleryItems);
+
+console.log(makeGalleryItems);
