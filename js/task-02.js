d5ebcd57d4649c200486fc7ac10e62bdb1223721
KasyanAndrey <andreykasyan88@gmail.com>
2021-05-26 14:27:36 +0300

    first commit hw-7 and done task-01

---
+++ js/task-02.js
@@ -0,0 +1,37 @@
+// const descrEl = document.querySelector('p');
+// // console.log(descrEl);
+
+// /* -------------------ul(categories) --------------------- */
+
+// const categoriesListEl = document.createElement('ul');
+// categoriesListEl.id = '#categories';
+// descrEl.after(categoriesListEl);
+
+// /* ------------------ li(categories) ---------------------- */
+
+// const categoriesItemEl = document.createElement('li');
+// categoriesItemEl.classList.add('item');
+// categoriesListEl.appendChild(categoriesItemEl);
+
+// /* ------------------ h2 ---------------------- */
+
+// const categoriesTitleEl = document.createElement('h2');
+
+// /* ------------------ ul subList----------------------- */
+
+// const subListEl = document.createElement('ul');
+
+// /* ------------------- li(underItem) ------------------- */
+
+// const underItemEl = document.createElement('li');
+
+// /* --------------------------------------------------- */
+// subListEl.appendChild(underItemEl);
+// // console.log(subListEl);
+
+// /* ---------------------------------------------- */
+
+// categoriesItemEl.append(categoriesTitleEl, subListEl);
+// // console.log(categoriesItemEl);
+
+// console.log(categoriesListEl);
